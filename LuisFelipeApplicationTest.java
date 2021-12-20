3f1214dba008306ed0686bee8236cbb7fbef3fa9
LuisFelipe-0401 <89155083+LuisFelipe-0401@users.noreply.github.com>
2021-12-20 17:20:03 -0300

    Create LuisFelipeApplicationTest.java

---
+++ LuisFelipeApplicationTest.java
@@ -0,0 +1,13 @@
+package com.LuisFelipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuisFelipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
